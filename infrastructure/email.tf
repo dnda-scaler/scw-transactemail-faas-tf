51f5e3a53b4d077d508806a910e1c37468f79986
Damien N DA <dnda@scaleway.com>
2023-01-02 14:03:02 +0100

    domain id

--- infrastructure/email.tf
+++ infrastructure/email.tf
@@ -4,11 +4,12 @@ variable "mail_domain_prefix" {
 }
 locals {
   mail_record="${var.mail_domain_prefix}.${var.dns_zone}"
+  domain_id=trim(scaleway_tem_domain.mail_domain.id,format("%s/%s",scaleway_tem_domain.mail_domain.region,"/"))
 }
 resource "scaleway_tem_domain" "mail_domain" {
   name = local.mail_record
 }
 
 output "domain_validation_url" {
-  value="https://api.scaleway.com/transactional-email/v1alpha1/regions/${scaleway_tem_domain.mail_domain.region}/domains/${scaleway_tem_domain.mail_domain.region}/check"
+  value="https://api.scaleway.com/transactional-email/v1alpha1/regions/${scaleway_tem_domain.mail_domain.region}/domains/${local.domain_id}/check"
 }
