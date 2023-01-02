1e084cb85a89397fba3f5eace9e4d74bcf29ef50
Damien N DA <dnda@scaleway.com>
2023-01-02 13:52:17 +0100

    domain validation url

--- infrastructure/email.tf
+++ infrastructure/email.tf
@@ -8,3 +8,7 @@ locals {
 resource "scaleway_tem_domain" "mail_domain" {
   name = local.mail_record
 }
+
+output "domain_validation_url" {
+  value="https://api.scaleway.com/transactional-email/v1alpha1/regions/${scaleway_tem_domain.mail_domain.region}/domains/${scaleway_tem_domain.mail_domain.region}/check"
+}
